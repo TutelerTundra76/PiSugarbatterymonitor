0acf79926d3984a8591a68ae359292d6842273ed
TutelerTundra76 <122492932+TutelerTundra76@users.noreply.github.com>
2024-05-19 07:51:15 -0500

    Update Install.sh
    removing unnecessary lines

--- Install.sh
+++ Install.sh
@@ -2,10 +2,7 @@
 
 cd ~
 sudo apt-get install libpng12-dev -y
-sudo apt-get install python-gpiozero -y
 sudo apt-get install python-pkg-resources python3-pkg-resources -y
-sudo apt-get install python-serial -y
-sudo git clone https://github.com/HoolyHoo/gbzbatterymonitor.git
-sudo chmod 755 /home/pi/gbzbatterymonitor/HHMonitorStart.sh
-sudo chmod 755 /home/pi/gbzbatterymonitor/Pngview/pngview
-sudo sed -i '/\"exit 0\"/!s/exit 0/\/home\/pi\/gbzbatterymonitor\/HHMonitorStart.sh \&\nexit 0/g' /etc/rc.local
+sudo git clone https://github.com/HoolyHoo/PiSugarbatterymonitor.git
+sudo chmod 755 /home/pi/PiSugarbatterymonitor/HHMonitorStart.sh
+sudo chmod 755 /home/pi/PiSugarbatterymonitor/Pngview/pngview
